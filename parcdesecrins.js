cdde43879f2a63e5bd785a0f55c60fd244dc57ca
Bart Stassen <hello@bartstassen.com>
2024-09-06 21:21:13 +0200

    chore: Optimize loading of custom markers and layers
    Refactor the code to optimize the loading of custom markers and layers on the map. Instead of using the `map.loadImage` function multiple times, use `await` and `map.addImage` to load and add the images in a more efficient way. This improves the performance and readability of the code.

--- parcdesecrins.js
+++ parcdesecrins.js
@@ -496,37 +496,37 @@ async function loadCustomMarkersAndLayers(dataGeoJson) {
 map.on("load", async () => {
   console.log("map on load");
 
-  const rw = await map.loadImage(googleBucketUrl + '/map/restaurant+walk.png');
-  map.addImage('restaurant+walk', rw);
+  // const rw = await map.loadImage(googleBucketUrl + '/map/restaurant+walk.png');
+  // map.addImage('restaurant+walk', rw);
 
-  const rc = await map.loadImage(googleBucketUrl + '/map/r-cluster.png');
-  map.addImage('r-cluster', rc);
+  // const rc = await map.loadImage(googleBucketUrl + '/map/r-cluster.png');
+  // map.addImage('r-cluster', rc);
 
-  const wc = await map.loadImage(googleBucketUrl + '/map/w-cluster.png');
-  map.addImage('w-cluster', wc);
-  getData();
+  // const wc = await map.loadImage(googleBucketUrl + '/map/w-cluster.png');
+  // map.addImage('w-cluster', wc);
+  // getData();
 
-  // map.loadImage(googleBucketUrl + "/map/restaurant+walk.png", (error, image) => {
-  //   if (error) throw error;
+  map.loadImage(googleBucketUrl + "/map/restaurant+walk.png", (error, image) => {
+    if (error) throw error;
 
-  //   map.addImage("restaurant+walk", image);
+    map.addImage("restaurant+walk", image);
 
-  //   map.loadImage(googleBucketUrl + "/map/r-cluster.png", (error, image) => {
-  //     if (error) throw error;
+    map.loadImage(googleBucketUrl + "/map/r-cluster.png", (error, image) => {
+      if (error) throw error;
 
-  //     map.addImage("r-cluster", image);
+      map.addImage("r-cluster", image);
 
-  //     map.loadImage(
-  //       googleBucketUrl + "/map/w-cluster.png", // Replace with your image URL
-  //       (error, image) => {
-  //         if (error) throw error;
+      map.loadImage(
+        googleBucketUrl + "/map/w-cluster.png", // Replace with your image URL
+        (error, image) => {
+          if (error) throw error;
 
-  //         map.addImage("w-cluster", image);
-  //         getData();
-  //       }
-  //     );
-  //   });
-  // });
+          map.addImage("w-cluster", image);
+          getData();
+        }
+      );
+    });
+  });
 
   // this adds cluster CIRCLES on map
 
