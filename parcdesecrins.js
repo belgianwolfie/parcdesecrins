80df40a2ff6036458e0c2b4371e32bf3b9aadacb
Bart Stassen <hello@bartstassen.com>
2024-09-07 22:27:32 +0200

    Refactor code to adjust list selection scroll behavior and clean up code in parcdesecrins.js

--- parcdesecrins.js
+++ parcdesecrins.js
@@ -733,15 +733,7 @@ function selectMapToList(element) {
   const listSelected = document.querySelector(`.uui-blogsection01_item[data-id="${element.properties.id}"]`);
   listSelected.classList.add('selected');
   listSelected.scrollIntoView({behavior: 'smooth', block: 'center'});
-  // Adjust the scroll position with an offset after a delay
-  setTimeout(() => {
-    const offset = 100;  // Adjust this value for your desired offset
-    window.scrollBy({
-      top: offset,  // Scroll up (negative value for offset)
-      left: 0,
-      behavior: 'smooth'
-    });
-  }, 300);  // The delay ensures scrollIntoView finishes first
+
 }
 
 function cleanSelection() {
