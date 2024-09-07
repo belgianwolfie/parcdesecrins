7e98b036161456cb7f55db124745e7dd37175863
Bart Stassen <hello@bartstassen.com>
2024-09-07 22:03:33 +0200

    Refactor code to improve map interaction and fix selected item bug

--- parcdesecrins.js
+++ parcdesecrins.js
@@ -728,7 +728,7 @@ function selectListToMap(item) {
 // function to communicate from MAP to LIST
 function selectMapToList(element) {
   cleanListSelection();
-  console.log("selected " + element);
+  console.log("selected " + element.id);
   const listSelected = document.querySelector(`.uui-blogsection01_item[data-id="${element.id}"]`);
   listSelected.classList.add('selected');
   listSelected.scrollIntoView({behavior: 'smooth', block: 'nearest'});
